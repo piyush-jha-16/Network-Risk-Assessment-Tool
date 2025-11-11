a0090c0b9e1470f8cf164677868181bea087e27d
Poojitagupta <poojitagupta29@gmail.com>
2025-11-11 22:06:13 +0530

    Report generator added

--- app.py
+++ app.py
@@ -569,8 +569,6 @@ def generate_report():
         return jsonify({'success': False, 'error': str(e)})
 
 
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port
     app.run(host="0.0.0.0", port=port, debug=True)
